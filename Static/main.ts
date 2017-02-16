4e066d3f8ca3e62e4a9f7da8b76d5da9f62d65d4
Pedro Ferreira <plferreira4@gmail.com>
2017-02-16 21:39:22 +0000

    - move the frontend code to a module

--- Static/main.ts
+++ Static/main.ts
@@ -1,140 +1,147 @@
-var SOCKET: WebSocket;
-var CHAT_LIST: HTMLUListElement;
-var CHAT_INPUT: HTMLInputElement;
-var USERNAME = '';
-
-
-interface Message {
-    message: string;
-    username: string;
-}
-
-
-const MessageType = {
-    getUsername : "U",  // U|(username)
-    textMessage : "M",  // M|(username)|(message)
-    userJoined  : "J",  // J|(username)
-    userLeft    : "L"   // L|(username)
-}
-
-
 window.onload = function () {
-
-    SOCKET = new WebSocket( "ws://" + window.location.host + "/chat", "chat" );
-    SOCKET.onopen = socketReady;
-    SOCKET.onmessage = function ( event: MessageEvent ) {
-        var type = event.data[ 0 ];
-
-        switch ( type ) {
-            case MessageType.getUsername:
-                var username = parseUsernameMessage( event.data );
-                associateUsername( username );
-                break;
-
-            case MessageType.textMessage:
-                var message = parseTextMessage( event.data );
-                addToList( message );
-                break;
-
-            case MessageType.userJoined:
-                var username = parseUsernameMessage( event.data );
-                userJoined( username );
-                break;
-
-            case MessageType.userLeft:
-                var username = parseUsernameMessage( event.data );
-                userLeft( username );
-                break;
-        }
-    };
-
-    init();
+    Chat.init();
 };
 
 
-function init() {
-    CHAT_LIST = <HTMLUListElement>document.getElementById( "ChatList" );
-    CHAT_INPUT = <HTMLInputElement>document.getElementById( "ChatInput" );
+module Chat {
 
-    CHAT_INPUT.onkeyup = function ( event ) {
+    interface Message {
+        message: string;
+        username: string;
+    }
 
-        // add a new message when the 'enter' key is pressed
-        if ( event.keyCode === 13 ) {
-            newMessage();
-        }
-    };
-}
-
-
-/**
- * Format: "U|(username)"
- */
-function parseUsernameMessage( data: string ) {
-    return data.slice( 2 );
-}
+    const MessageType = {
+        getUsername : "U",  // U|(username)
+        textMessage : "M",  // M|(username)|(message)
+        userJoined  : "J",  // J|(username)
+        userLeft    : "L"   // L|(username)
+    }
 
 
-/**
- * The server sends a message in the following format: "M|username|message".
- */
-function parseTextMessage( data: string ): Message {
-    var separator = data.indexOf( "|", 2 );
-    var username = data.slice( 2, separator );
-    var message = data.slice( separator + 1 );
-
-    return { username: username, message: message };
-}
+    var SOCKET: WebSocket;
+    var CHAT_LIST: HTMLUListElement;
+    var CHAT_INPUT: HTMLInputElement;
+    var USERNAME = '';
 
 
-/**
- * A new message was sent by the user. Add to the chat list and send it to the server as well.
- */
-function newMessage() {
-    var message = CHAT_INPUT.value;
-    CHAT_INPUT.value = '';
+    /**
+     * Initialize the chat.
+     */
+    export function init() {
+        SOCKET = new WebSocket( "ws://" + window.location.host + "/chat", "chat" );
+        SOCKET.onopen = socketReady;
+        SOCKET.onmessage = function ( event: MessageEvent ) {
+            var type = event.data[ 0 ];
 
-    SOCKET.send( "M|" + message );
-    addToList( {
-        message: message, username: USERNAME
-    });
-}
+            switch ( type ) {
+                case MessageType.getUsername:
+                    var username = parseUsernameMessage( event.data );
+                    associateUsername( username );
+                    break;
 
+                case MessageType.textMessage:
+                    var message = parseTextMessage( event.data );
+                    addToList( message );
+                    break;
 
-/**
- * Add a message to the chat list.
- */
-function addToList( message: Message ) {
-    var messageItem = document.createElement( "li" );
+                case MessageType.userJoined:
+                    var username = parseUsernameMessage( event.data );
+                    userJoined( username );
+                    break;
 
-    messageItem.innerText = message.username + ": " + message.message;
-    CHAT_LIST.appendChild( messageItem );
-}
+                case MessageType.userLeft:
+                    var username = parseUsernameMessage( event.data );
+                    userLeft( username );
+                    break;
+            }
+        };
 
+        CHAT_LIST = <HTMLUListElement>document.getElementById( "ChatList" );
+        CHAT_INPUT = <HTMLInputElement>document.getElementById( "ChatInput" );
 
-/**
- * Show a message saying the given user has joined the chat.
- */
-function userJoined( username: string ) {
-    addToList({ username: username, message: "Joined." });
-}
+        CHAT_INPUT.onkeyup = function ( event ) {
 
-
-/**
- * Show a message saying the given user has left the chat.
- */
-function userLeft( username: string ) {
-    addToList({ username: username, message: "Left." });
-}
-
-
-/**
- * The connection is opened, signal that to the server so it can send some initial information.
- */
-function socketReady() {
-    SOCKET.send( "R" );
-}
+            // add a new message when the 'enter' key is pressed
+            if ( event.keyCode === 13 ) {
+                newMessage();
+            }
+        };
+    }
 
 
-function associateUsername( username: string ) {
-    USERNAME = username;
+    /**
+     * Format: "U|(username)"
+     */
+    function parseUsernameMessage( data: string ) {
+        return data.slice( 2 );
+    }
+
+
+    /**
+     * The server sends a message in the following format: "M|username|message".
+     */
+    function parseTextMessage( data: string ): Message {
+        var separator = data.indexOf( "|", 2 );
+        var username = data.slice( 2, separator );
+        var message = data.slice( separator + 1 );
+
+        return { username: username, message: message };
+    }
+
+
+    /**
+     * A new message was sent by the user. Add to the chat list and send it to the server as well.
+     */
+    function newMessage() {
+        var message = CHAT_INPUT.value;
+        CHAT_INPUT.value = '';
+
+        SOCKET.send( "M|" + message );
+        addToList( {
+            message: message, username: USERNAME
+        });
+    }
+
+
+    /**
+     * Add a message to the chat list.
+     */
+    function addToList( message: Message ) {
+        var messageItem = document.createElement( "li" );
+
+        messageItem.innerText = message.username + ": " + message.message;
+        CHAT_LIST.appendChild( messageItem );
+    }
+
+
+    /**
+     * Show a message saying the given user has joined the chat.
+     */
+    function userJoined( username: string ) {
+        addToList({ username: username, message: "Joined." });
+    }
+
+
+    /**
+     * Show a message saying the given user has left the chat.
+     */
+    function userLeft( username: string ) {
+        addToList({ username: username, message: "Left." });
+    }
+
+
+    /**
+     * The connection is opened, signal that to the server so it can send some initial information.
+     */
+    function socketReady() {
+        SOCKET.send( "R" );
+    }
+
+
+    /**
+     * Set this user name.
+     */
+    function associateUsername( username: string ) {
+        USERNAME = username;
+    }
 }
