50c27fd914d6aa20267f0d4f85fdd6161a118d4e
Pedro Ferreira <plferreira4@gmail.com>
2018-10-28 01:23:08 +0100

    Update all the dependencies to the latest versions.
    - Swift to 'v4.2'.
    - Kitura dependencies.
    - Typescript to 'v3.1'.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
@@ -7,10 +7,10 @@ let package = Package(
         .executable( name: "chat", targets: [ "chat" ] )
     ],
     dependencies: [
-        .package( url: "https://github.com/IBM-Swift/Kitura.git",                       .upToNextMajor( from: "2.1.0" ) ),
+        .package( url: "https://github.com/IBM-Swift/Kitura.git",                       .upToNextMajor( from: "2.5.0" ) ),
         .package( url: "https://github.com/IBM-Swift/HeliumLogger.git",                 .upToNextMajor( from: "1.7.0" ) ),
-        .package( url: "https://github.com/IBM-Swift/Kitura-WebSocket.git",             .upToNextMajor( from: "1.0.0" ) ),
-        .package( url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", .upToNextMajor( from: "1.8.0" ) )
+        .package( url: "https://github.com/IBM-Swift/Kitura-WebSocket.git",             .upToNextMajor( from: "2.1.0" ) ),
+        .package( url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", .upToNextMajor( from: "1.10.0" ) )
     ],
     targets: [
         .target(
