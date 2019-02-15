7a965a81dad16024af4d57640813e5424cc64456
Pedro Ferreira <plferreira4@gmail.com>
2019-02-15 17:31:14 +0000

    Differentiate between the different kinds of messages we can receive from the server.
    - Only show the message part in the table view.

--- Client_iOS/chat/Chat.swift
+++ Client_iOS/chat/Chat.swift
@@ -2,14 +2,14 @@ import Foundation
 import Starscream
 
 
-protocol ChatDelegate {
+protocol ChatDelegate: class {
     func received(message: String) -> Void
 }
 
 
 class Chat: WebSocketDelegate {
     var socket: WebSocket
-    var delegate: ChatDelegate?
+    weak var delegate: ChatDelegate!
     
     init(_ serverUrl: String) {
         let url = URL(string: serverUrl)!
@@ -37,11 +37,64 @@ class Chat: WebSocketDelegate {
     
     
     func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {
-        self.delegate?.received(message: text)
+        let code = text.prefix(1)
+
+        switch code {
+        case "U":
+            self.userName(text)
+        
+        case "C":
+            self.connectedCount(text)
+            
+        case "M":
+            self.receivedMessage(text)
+            
+        case "J":
+            self.userJoined(text)
+        
+        case "L":
+            self.userLeft(text)
+            
+        default:
+            break
+        }
+        
     }
     
     
     func websocketDidReceiveData(socket: WebSocketClient, data: Data) {
+        
+    }
+    
+    
+    func userName(_ text: String) {
+        
+    }
+    
+    
+    func connectedCount(_ text: String) {
+        
+    }
+    
+    /**
+     * Format: M|(time)|(username)|(message)
+     */
+    func receivedMessage(_ text: String) {
+        let split = text.components(separatedBy: "|")
+        let time = split[1]
+        let username = split[2]
+        let message = split[3]
+
+        self.delegate.received(message: message)
+    }
+    
+    
+    func userJoined(_ text: String) {
+        
+    }
+    
+    
+    func userLeft(_ text: String) {
         
     }
 }
