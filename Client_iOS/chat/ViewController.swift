b8bd2ebe8cf54f0bb46468131aea0c44e06d5cff
Pedro Ferreira <plferreira4@gmail.com>
2019-02-15 16:24:11 +0000

    Add the basic UI elements to the main storyboard.
    - Add a reference to the view controller (outlets).
    - Setup the table view to display a test list.

--- Client_iOS/chat/ViewController.swift
+++ Client_iOS/chat/ViewController.swift
@@ -1,9 +1,16 @@
 import UIKit
 import Starscream
 
-class ViewController: UIViewController, WebSocketDelegate {
+class ViewController: UIViewController, WebSocketDelegate, UITableViewDataSource, UITableViewDelegate {
 
     var socket: WebSocket!
+    var messages = ["test1", "test2", "test3"]
+
+    @IBOutlet weak var messagesTableView: UITableView!
+    @IBOutlet weak var connectedCountLabel: UILabel!
+    @IBOutlet weak var optionsButton: UIButton!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var inputTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +25,20 @@ class ViewController: UIViewController, WebSocketDelegate {
     deinit {
         self.socket.disconnect(forceTimeout: 0)
         self.socket.delegate = nil
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCell", for: indexPath)
+        let message = self.messages[indexPath.row]
+        
+        cell.textLabel?.text = message
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
     }
     
     
