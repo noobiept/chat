98af945550645166cf70b6576f240806b3afb634
Pedro Ferreira <plferreira4@gmail.com>
2019-03-23 17:52:40 +0000

    Show a different background color on the user's on messages (in watchOS).

--- Client_iOS/watch Extension/Views/MessageRow.swift
+++ Client_iOS/watch Extension/Views/MessageRow.swift
@@ -4,12 +4,21 @@ import WatchKit
 class MessageRow: NSObject {
 
     @IBOutlet var text: WKInterfaceLabel!
+    @IBOutlet var group: WKInterfaceGroup!
 
 
     func update( _ message: Message ) {
-        let color = getUserColor( message.username, darker: false )
+        if message.type == .user {
+            self.group.setBackgroundColor( .gray )
+            self.text.setTextColor( .white )
+        }
 
-        self.text.setTextColor( color )
+        else {
+            let color = getUserColor( message.username, darker: false )
+            self.text.setTextColor( color )
+            self.group.setBackgroundColor( nil )
+        }
+
         self.text.setText( message.message )
     }
 }
